2ec3a27612f81446c5a2a64aa1ea225895e38c1c
danielcampos <ddregalo@gmail.com>
2020-04-19 22:41:16 +0100

    Create Navbar component

---
+++ svelte-app/src/components/common/NavBar.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let username;
+</script>
+
+<header id="nav-user">
+    <p class="username">book tracker</p>
+    <p class="username">welcome | {username}</p>
+</header>
+
+<style>
+    header {
+        background-color: #ff7869;
+        text-align: left;
+        letter-spacing: 3px;
+        padding: 0.1em 4em 0.1em 4em;
+        margin: 0 auto;
+        width: 100%;
+    }
+
+    .username {
+        font-size: 0.9em;
+        color: white;
+    }
+
+    #nav-user {
+        display: inline-grid;
+        grid-template-columns: 70% 30%;
+    }
+</style>
