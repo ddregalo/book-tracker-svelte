515cbe615d5fc8604a5be84bdbe12999d4f439b0
danielcampos <ddregalo@gmail.com>
2020-04-19 22:41:31 +0100

    Create button component

---
+++ svelte-app/src/components/common/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="button-container">
+    <slot/>
+</div>
+
+<style>
+ .button-container { 
+     max-width: 8em;
+     max-height: 1.2em;
+     border-radius: 12px;
+     cursor: pointer;
+     background-color: aqua;
+ }
+</style>
