62e049013e6d1a73b0188bfdc3d4f3a3901bdc3c
danielcampos <ddregalo@gmail.com>
2020-04-19 22:42:00 +0100

    Create BookGrid component

---
+++ svelte-app/src/components/BookGrid.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let books = [];
+</script>
+
+<div class="books-container">
+    {#each books as book}
+        <p>Title: {book.title}</p>
+        <p>Author: {book.author}</p>
+        <p>Rating: {book.rating}</p>        
+        <hr />
+    {/each}
+</div>
+
+<style>
+
+</style>
