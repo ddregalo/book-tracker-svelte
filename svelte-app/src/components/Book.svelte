a367eb65c29297a2766c8f259587b009b6e78baf
danielcampos <ddregalo@gmail.com>
2020-04-20 18:10:47 +0100

    Add Book component

---
+++ svelte-app/src/components/Book.svelte
@@ -0,0 +1,38 @@
+<script>
+    export let book = {};
+
+    function isValidUrl(url) {
+        return url && /http.+\.(jpg|jpeg|png|gif)$/.test(url);
+    }
+</script>
+
+<a href="#" class="book-card">
+    <img class="cover-img" src={isValidUrl(book.coverUrl) ? book.coverUrl : ''} />
+    <div class="book-details">
+        <p class="title">{ book.title || 'title not found' }</p>
+        <p class="author">{ book.author || 'author not found' }</p>
+    </div>
+    
+</a>
+
+<style>
+    img {
+        height: 150px;;
+        width: auto;
+    }
+    
+    .book-card {
+        font: "Manrope";
+        font-size: 0.7em;
+    }
+
+    .book-details {
+
+    }
+
+    #books-container {
+        display: grid;
+
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+</style>
